77321b2a43dfe970c4e2710e14499f6f478d9193
HungTranXD <105595047+HungTranXD@users.noreply.github.com>
2023-09-14 08:24:14 +0700

    first commit

---
+++ AzureExam/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add CORS:
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+            policy.AllowAnyMethod();
+            policy.AllowAnyHeader();
+        }
+    );
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+// Add connection to databbase.
+var connectionString = builder.Configuration.GetConnectionString("T2204M_Azure_Exam");
+/*builder.Services.AddDbContext<WebAPI.Entities.T2204mAspnetApiContext>(
+    options => options.UseSqlServer(connectionString)
+);*/
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
